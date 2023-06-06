99473f96c41af39b221f7746d4d38292b058f9dc
krishpranav <krisna.pranav@gmail.com>
2023-06-06 15:10:54 +0530

    index: file[file functionalities]

--- index/file.go
+++ index/file.go
@@ -1,1 +1,17 @@
+// @filename: file.go
+// @author: Krisna Pranav
+// @license: Copyright (c) 2023, godzillaframework, Krisna Pranav
+
 package index
+
+import (
+	"io"
+	"io/fs"
+)
+
+// file[Stat]
+type File interface {
+	io.ReadWriteCloser
+
+	Stat() (fs.FileInfo, error)
+}
